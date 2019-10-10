d933955458d3e81a1571bdc4cb451df9ea934014
Victor Tereschenko <sibvic@gmail.com>
2019-10-10 18:00:55 +0600

    fixed keys

--- InjectOptions.cs
+++ InjectOptions.cs
@@ -6,13 +6,13 @@ namespace ProfitRobots.MQ4Inject
     [Verb("inject", HelpText = "Injects includes into the code.")]
     class InjectOptions
     {
-        [Option("--sources", Required = true, HelpText = "Path to sources")]
+        [Option("sources", Required = true, HelpText = "Path to sources")]
         public string Sources { get; set; }
 
-        [Option("--source-file", Required = true, HelpText = "Source file.")]
+        [Option("source-file", Required = true, HelpText = "Source file.")]
         public string SourceFile { get; set; }
 
-        [Option("--target-file", HelpText = "Target file. The save as source by default.")]
+        [Option("target-file", HelpText = "Target file. The save as source by default.")]
         public string TargetFile { get; set; }
     }
 }
